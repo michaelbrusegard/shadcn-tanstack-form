7013d426d0db712e5ed3bf1902d8a835bc89028b
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-09-10 14:16:14 +0200

    add basefiles and auto deployment of registry

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isGithubActions && repoName ? `/${repoName}` : '',
+  assetPrefix: isGithubActions && repoName ? `/${repoName}/` : '',
 };
 
 export default nextConfig;
---
+++ registry/default/ui/spinner.tsx
@@ -0,0 +1,31 @@
+import { Loader2Icon, type LucideProps } from 'lucide-react';
+
+import { cva, type VariantProps } from "class-variance-authority"
+
+const spinnerVariants = cva('text-muted-foreground animate-spin',
+  {
+    variants: {
+      size: {
+        default: 'h-6 w-6',
+        sm: 'h-4 w-4',
+        lg: 'h-8 w-8',
+        xl: 'h-10 w-10',
+        none: '',
+      },
+    },
+    defaultVariants: {
+      size: 'default',
+    },
+  });
+
+function Spinner({
+  className,
+  size,
+  ...props
+}: LucideProps & VariantProps<typeof spinnerVariants>) {
+  return (
+    <Loader2Icon className={spinnerVariants({ size, className })} {...props} />
+  );
+}
+
+export { Spinner };
